94a7434286b8b94b3e336484cc6a44abf36f0124
Ghazal-UofU <118317425+Ghazal-UofU@users.noreply.github.com>
2023-01-05 11:47:35 -0700

    Add files via upload
    # built-in data structures (containers): serving to work better with data
    #list: putting different objects from different data types, even putting lists in a list with whatever the pattern is. It is based on the order you put in it. It has indexes.
    #It can be changed (add, remove, and modification of elements). It's element can be diverse in terms of type.

---
+++ Data structures/lists.py
@@ -0,0 +1,57 @@
+# built-in data structures (containers): serving to work better with data
+#list: putting different objects from different data types, even putting lists in a list with whatever the pattern is. It is based on the order you put in it. It has indexes.
+#It can be changed (add, remove, and modification of elements). It's element can be diverse in terms of type.
+
+
+#creating list
+
+q= [100, 5, 4, 's']
+print(q)
+print(type(q))
+
+
+print('----------------------------------------------------------')
+
+
+t=[[1,2, 's'], q, 9]    #list in list, and invoking list in list
+
+print(t)
+print(type(t))
+
+
+
+print('----------------------------------------------------------')
+
+
+f= list ([2, 3, 4])         #functional when we learn to work with other data structures, aiming to convert to another data structure
+print(f)
+
+
+print('----------------------------------------------------------')
+
+print(list ('kjwdfkrehgkhreg94398578584'))
+
+
+print('----------------------------------------------------------')
+
+
+o= list ('kjwdfkrehgkhreg94398578584')
+
+print(o)
+
+print('----------------------------------------------------------')
+
+d=[]
+print(type(d))
+
+print('----------------------------------------------------------')
+
+print(len(d))
+print(len(o))
+print(len(t))       #only count the elements between , ,  and not the inner ones
+
+
+
+
+
+
