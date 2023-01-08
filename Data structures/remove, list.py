de0ad8f8f252c7be74ba97903b74c08fb9d4e3e0
Ghazal Abdollahi <118317425+Ghazal-UofU@users.noreply.github.com>
2023-01-08 09:37:52 -0700

    Add files via upload
    #remove from the list

---
+++ Data structures/remove, list.py
@@ -0,0 +1,28 @@
+#8.1.2023
+#Ghazal
+
+#remove from the list
+
+r= [1, 2, 3, 3, 5, 66, 66, ['a', 700]]
+
+r.remove(66)        #remove the first element which is 66
+print(r)
+
+
+r.remove (['a', 700])
+print(r)
+
+
+r.pop(0)    #it removes the value within index 0
+print(r)
+
+
+r.pop ()    #if u don't get a value to it, it removes the last itemS
+print(r)
+
+r.clear()   #it clears the whole list
+print(r)
+
+
+
+
