77ad42eeda66c6e2aca16516ebc2b2e7b25d6db3
Ghazal Abdollahi <118317425+Ghazal-UofU@users.noreply.github.com>
2023-01-08 13:50:02 -0700

    Add files via upload
    # putting odd and even into separate lists

---
+++ Data structures/odd even.py
@@ -0,0 +1,22 @@
+#8.1.2023
+#Ghazal
+
+
+# putting odd and even into seprate lists
+
+c= [4, 2, 6, 7, 88, 33, 555, 3, 9999]
+
+
+e=[]
+o=[]
+
+for i in c:
+    if i%2==0:          #[4, 2, 6, 88]
+        e.append(i)
+
+    else:
+        o.append(i)     #[7, 33, 555, 3, 9999]
+
+
+print('even list=', e)    
+print('odd list=', o)
