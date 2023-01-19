18df3aaaddfbe85fc4ebff98b0ab83733a8a79fc
Ghazal Abdollahi <118317425+Ghazal-UofU@users.noreply.github.com>
2023-01-19 11:41:06 -0700

    Add files via upload
    # Jaccard Similarity

---
+++ Data mining/Jaccard Similarity.py
@@ -0,0 +1,18 @@
+# 19. 1. 2023
+# Jaccard Similarity
+# Ghazal
+
+
+
+def jaccard_set(set1, set2):
+    intersection = set(set1).intersection(set(set2))      # Define Jaccard Similarity function for two sets
+    print('intersection is= ', intersection)
+    union = set(set1 + set2)
+    print('union is = ', union)
+    print(float(len(intersection) / len(union)))
+# Define two sets 
+a = [0, 1, 2, 5, 6]
+b = [0, 2, 3, 4, 5, 7, 9]
+
+# Find Jaccard Similarity between the two sets 
+jaccard_set(a, b)
